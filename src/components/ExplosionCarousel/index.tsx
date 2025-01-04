d0528d3447cfe45cccfc6fa81a93452a370eeab7
Gabriel Fresatto <t_gabriel.valadao@hapvida.com.br>
2025-01-03 22:30:29 -0300

    feat: quase la

---
+++ src/components/ExplosionCarousel/index.tsx
@@ -0,0 +1,83 @@
+import { motion, Variants } from "framer-motion";
+import { v4 as uuidv4 } from "uuid";
+
+import { Container } from "./styles";
+
+const phrases = ["Desenvolvedor Front-End", "React JS", "React Native", "Next"];
+
+const ExplosionCarousel = () => {
+  const containerVariants: Variants = {
+    visible: {
+      transition: {
+        staggerChildren: 2,
+        repeat: Infinity,
+      },
+    },
+  };
+
+  const phrasesSeparatedByLetter = phrases.map((phrase) => phrase.split(""));
+
+  const getRandomPosition = () => {
+    const radius = 100;
+    const angle = Math.random() * 2 * Math.PI; // Gera ângulo aleatório (0 a 360 graus)
+    const x = radius * Math.cos(angle); // Posição X
+    const y = radius * Math.sin(angle); // Posição Y
+    return { x, y, left: x + "px", top: y + "px" };
+  };
+
+  const emotionElements = phrasesSeparatedByLetter.map((arr) => {
+    const emotionLetter = arr.map((letter) => {
+      const { left, top } = getRandomPosition();
+
+      const letterVariant: Variants = {
+        hidden: {
+          opacity: 0,
+          left,
+          top,
+        },
+        visible: {
+          opacity: 1,
+          left: 0,
+          top: 0,
+          transition: {
+            duration: 4,
+            repeat: 2,
+            repeatType: "reverse",
+          },
+        },
+      };
+
+      return {
+        id: uuidv4(),
+        letter,
+        letterVariant,
+      };
+    });
+
+    return { arrays: emotionLetter, id: uuidv4() };
+  });
+
+  return (
+    <Container style={{ overflow: "visible" }}>
+      {emotionElements.map(({ arrays, id }) => {
+        return (
+          <motion.p key={id} style={{ position: "relative" }}>
+            {arrays.map(({ letter, letterVariant, id }) => (
+              <motion.span
+                key={id}
+                variants={letterVariant}
+                initial="hidden"
+                animate="visible"
+                style={{ position: "relative" }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+          </motion.p>
+        );
+      })}
+    </Container>
+  );
+};
+
+export default ExplosionCarousel;
--- src/pages/Initial/index.tsx
+++ src/pages/Initial/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Container } from "./styles";
 import TextCarousel from "../../components/TextCarousel";
+import ExplosionCarousel from "../../components/ExplosionCarousel";
 
 const Initial: React.FC = () => {
   return (
     <Container>
       <h1>Gabriel Fresatto</h1>
-      <TextCarousel />
+      {/* <TextCarousel /> */}
+      <ExplosionCarousel />
     </Container>
   );
 };
