86f57ebc2236349451bf06de3769a2fd3b954319
Gabriel Fresatto <t_gabriel.valadao@hapvida.com.br>
2025-01-19 17:07:44 -0300

    feat: escondendo os depoimentos e adicionando o favicon

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -149,7 +149,7 @@ const About: React.FC = () => {
           </SkillGroup>
         </SkillsSection>
       </InfoSection>
-      <TestimonialsSection />
+      {/* <TestimonialsSection /> */}
     </AnimatedPageContainer>
   );
 };
